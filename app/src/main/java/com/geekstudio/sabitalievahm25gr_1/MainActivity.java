3961fb29715c4e5a12f8f63e58884127be80c343
SabitalievaNuskaiym <77196324+Sabitalieva@users.noreply.github.com>
2022-10-19 23:54:35 +0600

    Initial commit

---
+++ app/src/main/java/com/geekstudio/sabitalievahm25gr_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.geekstudio.sabitalievahm25gr_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
